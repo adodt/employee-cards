99f1270019b2f82fec7aa809333f3456ca72f109
Gator <allidodt@gmail.com>
2022-10-22 16:22:45 -0700

    manager test

--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,17 +1,15 @@
-//class 
+//Manager constructor
+const Manager = require('../lib/Manager');
 
-//employee name
+//create Manager object
+test('create Manger object', () => {
+    const manager = new Manager('Alli', 222, 'allidodt@gmail.com', 1);
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
 
-//employee ID
+//tests Role
+test('get role of employee', () => {
+    const manager = new Manager('Alli', 222, 'allidodt@gmail.com', 1);
+    expect(manager.getRole()).toEqual("Manager");
 
-//employee email
-
-//office number
-
-////getName()
-
-//getId()
-
-//getEmail()
-
-//getRole() overridden to return 'Manager'
+});
