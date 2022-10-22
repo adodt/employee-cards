381349f53923f4ba808e51afbf350ea1cb045655
Gator <allidodt@gmail.com>
2022-10-22 11:25:55 -0700

    properties for Employee class test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,35 @@
-//constructor 
+//require Employee constructor 
+const Employee = require('../lib/Employee')
 
-//employee name
+//Employee object
+test('creates employee object', () => {
+    const employee = new Employee('Alli', 222, 'allidodt@gmail.com');
 
-//employee ID
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+});
 
-//employee email
+//tests employee name
+test('get employee name', () => {
+    const employee = new Employee('Alli', 222, 'allidodt@gmail.com');
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+//tests employee ID
+test('get employee ID', () => {
+    const employee = new Employee('Alli', 222, 'allidodt@gmail.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+
+});
+
+//tests employee email
+test('get employee email', () => {
+    const employee = new Employee('Alli', 222, 'allidodt@gmail.com');
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString));
+
+});
+
 
 ////getName()
 
