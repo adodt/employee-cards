c3737184a7c6bd114e18bc63558a5e1164fd17e5
Gator <allidodt@gmail.com>
2022-10-23 11:25:15 -0700

    intern card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,6 +1,6 @@
 //HTML page
-const generateTeamPage = function(employeeCards) {
-    return`
+const generateTeamPage = function (employeeCards) {
+    return `
     <!DOCTYPE html>
     <html lang="en">
     
@@ -40,16 +40,18 @@ const generateTeamPage = function(employeeCards) {
 const generateManager = function (manager) {
     return `
     <div class="col-4 mt-4">
-    <div class="cardHeader">
-        <h3>${manager.name}</h3>
-        <h4>Manager</h4><i class="material-icons">content_paste</i>
+        <div class="card h-100">
+            <div class="cardHeader">
+                <h3>${manager.name}</h3>
+                <h4>Manager</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="cardBody">
+                <p class="id">ID number: ${manager.id}</p>
+                <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p class="officeNumber">Office number: ${manager.officeNumber}</p>
+            </div>
+        </div>
     </div>
-    <div class="cardBody">
-        <p class="id">ID number: ${manager.id}</p>
-        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-        <p class="officeNumber">Office number: ${manager.officeNumber}</p>
-    </div>
-</div>
     `
 }
 
@@ -57,17 +59,40 @@ const generateManager = function (manager) {
 const generateEngineer = function (engineer) {
     return `
     <div class="col-4 mt-4">
-    <div class="cardHeader">
-        <h3>${engineer.name}</h3>
-        <h4>Engineer</h4><i class="material-icons">content_paste</i>
+        <div class="card h-100">
+            <div class="cardHeader">
+                <h3>${engineer.name}</h3>
+                <h4>Engineer</h4><i class="material-icons">content_paste</i>
+            </div>
+            <div class="cardBody">
+                <p class="id">ID number: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${engineer.email}"></a></p>
+                <p class="github">Github profile: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+            </div>
+        </div>
     </div>
-    <div class="cardBody">
-        <p class="id">ID number: ${manager.id}</p>
-        <p class="email">Email: <a href="mailto:${engineer.email}"></a></p>
-        <p class="github">Github profile: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
-    </div>
-</div>
     `
 }
+
+//Intern Card
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+        <div class="card h-100">
+            <div class="cardHeader">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="cardBody">
+                <p class="id">ID number: ${engineer.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}"></a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+        </div>
+    </div>
+    `
+};
+
+
 
 module.exports = generateHTML;
