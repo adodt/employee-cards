26ff66afe5d6a45f583efba2589e2d1dd6156fb6
Gator <allidodt@gmail.com>
2022-10-23 14:23:59 -0700

    debug bootstrap link and css link

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,43 +1,4 @@
-//HTML page
-const generateTeamPage = function (employeeCards) {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Profile</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Serif&display=swap" rel="stylesheet">
-        <script src="https://kit.fontawesome.com/d209825068.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="./dist/style.css">
-    </head>
-    
-    <body>
-    <header>
-        <nav class="navbar">
-            <div class="h1 w-100 text-center">THE TEAM</div>
-        </nav>
-    </header>
-    <main>
-        <div class="cardContainer">
-            <div class="row justify-content-center" id="teamCards">
-                ${employeeCards}
-                </div>
-                </main>
-            
-            </body>
-            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-                crossorigin="anonymous"></script>
-            
-            </html>
-    `
-}
+
 
 //Manager card
 const generateManager = function (manager) {
@@ -123,6 +84,54 @@ generateHTML = (data) => {
     const generateTeam = generateTeamPage(employeeCards);
     return generateTeam;
 };
+
+//HTML page
+const generateTeamPage = function (employeeCards) {
+    return`
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Noto+Serif&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d209825068.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header>
+            <nav class="navbar">
+                <div class="h1 w-100 text-center">THE TEAM</div>
+            </nav>
+        </header>
+        <main>
+            <div class="cardContainer">
+                <div class="row justify-content-center" id="teamCards">
+                ${employeeCards}
+                </div>
+            </div>
+        </main>
+            
+    </body>
+   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+                integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+                crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+                integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+                crossorigin="anonymous"></script>       
+    </html>
+
+    `;
+}
 
 
 module.exports = generateHTML;
