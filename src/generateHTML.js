e264a4db45a889bb741afa88eb72c9b86c527c9d
Gator <allidodt@gmail.com>
2022-10-23 14:28:32 -0700

    added npm start and deleted test.html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -94,7 +94,7 @@ const generateTeamPage = function (employeeCards) {
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Team Profile</title>
+    <title>DREAM TEAM</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
     integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
