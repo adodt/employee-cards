eb2090c37e330814e6b7852ad95c1865677654a4
Gator <allidodt@gmail.com>
2022-10-23 11:17:07 -0700

    engineer card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,30 +1,73 @@
 //HTML page
-const generateTeamPage = function (employeeCards) {
+const generateTeamPage = function(employeeCards) {
     return`
     <!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Team Profile</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link rel="stylesheet" href="style.css">
-      </head>
-
-      <body>
-      <header> 
-        <nav class="navbar" id=
-
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+            integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    
+    <body>
+        <header>
+            <nav class="navbar" id="navbar">
+                <div class="navbar-brand mb-0 h1 w-100 tex-center" id="navbar-text">THE TEAM</div>
+            </nav>
+        </header>
+        <main>
+            <div class="cardContainer">
+                <div class="row justify-content-center" id="teamCards">
+                    ${employeeCards}
+                </div>
+            </div>
+        </main>
+    
+    </body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </html>
     `
 }
 
 //Manager card
 const generateManager = function (manager) {
     return `
-    <div calss="col
+    <div class="col-4 mt-4">
+    <div class="cardHeader">
+        <h3>${manager.name}</h3>
+        <h4>Manager</h4><i class="material-icons">content_paste</i>
+    </div>
+    <div class="cardBody">
+        <p class="id">ID number: ${manager.id}</p>
+        <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+        <p class="officeNumber">Office number: ${manager.officeNumber}</p>
+    </div>
+</div>
     `
 }
 
+//Engineer Card
+const generateEngineer = function (engineer) {
+    return `
+    <div class="col-4 mt-4">
+    <div class="cardHeader">
+        <h3>${engineer.name}</h3>
+        <h4>Engineer</h4><i class="material-icons">content_paste</i>
+    </div>
+    <div class="cardBody">
+        <p class="id">ID number: ${manager.id}</p>
+        <p class="email">Email: <a href="mailto:${engineer.email}"></a></p>
+        <p class="github">Github profile: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+    </div>
+</div>
+    `
+}
 
 module.exports = generateHTML;
