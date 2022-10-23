bdaa2773d8db834c91a981fa601cbd472b16452f
Gator <allidodt@gmail.com>
2022-10-23 10:57:57 -0700

    laying out HTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,2 +1,30 @@
+//HTML page
+const generateTeamPage = function (employeeCards) {
+    return`
+    <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team Profile</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+      <link rel="stylesheet" href="style.css">
+      </head>
+
+      <body>
+      <header> 
+        <nav class="navbar" id=
+
+    `
+}
+
 //Manager card
-const generate
+const generateManager = function (manager) {
+    return `
+    <div calss="col
+    `
+}
+
+
+module.exports = generateHTML;
